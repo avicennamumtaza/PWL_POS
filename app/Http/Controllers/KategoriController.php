7c12ab34157a6e33d89af31260e99b338778c2b4
avicennamumtaza <avislamongan@gmail.com>
2024-03-06 23:48:35 +0700

    fix: update controller for level uri to update and delete a row, also show the entire row of data/s in m_level database table

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,12 +9,21 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at' => now(),
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'insert data berhasil.';
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now(),
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'insert data berhasil.';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        // return 'update data berhasil' . PHP_EOL . "Jumlah data yang diupdate ada $row baris.";
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'delete data berhasil' . PHP_EOL . "Jumlah data yang dihapus ada $row baris.";
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
     }
 }
