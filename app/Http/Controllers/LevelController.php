93a9af3bbe8b57a863660b8e326fa2513d73ec8a
avicennamumtaza <avislamongan@gmail.com>
2024-03-06 23:19:25 +0700

    feat: add a function in LevelController to show entire data of m_level database table and add a view for existing function of LevelController

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -15,7 +15,10 @@ class LevelController extends Controller
         // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
         // return 'update data berhasil.' . PHP_EOL . "Jumlah data yang diupdate ada $row baris.";
 
-        $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
-        return 'delete data berhasil.' . PHP_EOL . "Jumlah data yang dihapus ada $row baris.";
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'delete data berhasil.' . PHP_EOL . "Jumlah data yang dihapus ada $row baris.";
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data'=>$data]);
     }
 }
