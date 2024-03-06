ac8d27fcf8c5daf7ec60c63358fb4024a4341aa5
avicennamumtaza <avislamongan@gmail.com>
2024-03-06 23:10:13 +0700

    fix: update controller for level uri to delete a row of data in m_level database table

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -11,7 +11,11 @@ class LevelController extends Controller
     {
         // DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
         // return 'insert data berhasil.';
-        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
-        return 'update data berhasil.' . PHP_EOL . "Jumlah data yang diupdate ada $row baris.";
+
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'update data berhasil.' . PHP_EOL . "Jumlah data yang diupdate ada $row baris.";
+
+        $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        return 'delete data berhasil.' . PHP_EOL . "Jumlah data yang dihapus ada $row baris.";
     }
 }
