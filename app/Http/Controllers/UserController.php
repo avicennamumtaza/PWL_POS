8f320a411ba7bc2dca90a76de74152adc568a5a5
avicennamumtaza <avislamongan@gmail.com>
2024-03-07 00:20:57 +0700

    feat: add model for user object, view to show entire rows of user table, and controllers which has some function to insert, update, delete, and select from user table in database

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // add a user data with Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 3,
+        // ];
+        // UserModel::insert($data);
+        
+        // // change a user data with Eloquent Model
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
+
+        // delete a user data with Eloquent Model
+        // UserModel::where('username', 'customer-1')->delete();
+
+        // try to access UserModel
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
