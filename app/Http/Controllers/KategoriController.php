17bf577484ec9563f71ad8af8260c14799280b82
avicennamumtaza <avislamongan@gmail.com>
2024-03-27 13:43:07 +0700

    feat: add create update delete button in kategori data table, entire of jobsheet 5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index() // CRUD DATA IN DATABASE WITH QUERY BUILDER
+    public function index(KategoriDataTable $dataTable) // CRUD DATA IN DATABASE WITH QUERY BUILDER
     {
         // $data = [
         //     'kategori_kode' => 'SNK',
@@ -23,7 +25,51 @@ class KategoriController extends Controller
         // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
         // return 'delete data berhasil' . PHP_EOL . "Jumlah data yang dihapus ada $row baris.";
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        // $data = DB::table('m_kategori')->get();
+        // return view('kategori', ['data' => $data]);
+            
+        return $dataTable->render('kategori.index');
     }
+
+    public function create() {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request) {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori
+        ]);
+        return redirect('/kategori');
+    }
+
+    public function edit($id){
+        $kategori = KategoriModel::find($id);
+        return view('kategori.edit', ['data' => $kategori]);
+    }
+
+    public function update(Request $request, $id){
+        $kategori = KategoriModel::find($id);
+        $kategori->kategori_kode = $request->kategori_kode;
+        $kategori->kategori_nama = $request->kategori_nama;
+        $kategori->save();
+        return redirect('/kategori');    
+    }
+
+    public function destroy($id) {
+        // Cek apakah ada entri di m_barang yang memiliki kategori_id yang sama dengan $id
+        // $barangCount = KategoriModel::where('kategori_id', $id)->count();
+    
+        // // Jika ada barang yang terkait dengan kategori, berikan pesan error
+        // if ($barangCount > 0) {
+        //     return redirect('/kategori')->with('error', 'Tidak dapat menghapus kategori karena masih terdapat barang terkait.');
+        // }
+    
+        // Jika tidak ada barang terkait, hapus kategori
+        KategoriModel::find($id)->delete();
+    
+        // Redirect ke halaman kategori
+        return redirect('/kategori')->with('success', 'Kategori berhasil dihapus.');
+    }
+    
 }
---
+++ resources/views/kategori/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Kategori')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Kategori')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Manage Kategori
+            </div>
+            <div class="card-body">
+                <a class="btn btn-primary" href={{url("/kategori/create")}}>
+                    Tambah Kategori
+                </a>
+            </div>
+            <div class="card-body"> 
+                {{ $dataTable->table() }}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    {{ $dataTable->scripts() }}
+@endpush
