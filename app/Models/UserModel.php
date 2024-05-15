1242f46e1592b300288c05ff6676a1b3718d4d0d
avicennamumtaza <avislamongan@gmail.com>
2024-05-15 10:38:48 +0700

    fix: update user model and controller for auth needs

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,17 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+// use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use function Laravel\Prompts\password;
 
-class UserModel extends Model implements AuthenticatableContract
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
     use \Illuminate\Auth\Authenticatable;
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
